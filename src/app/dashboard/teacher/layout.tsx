c5c2a9a97aea78655893c617c1f477d7b19cdf23
nooby6 <giftaudrine1@gmail.com>
2025-01-23 17:35:20 +0300

    Add TeacherPage and DashboardLayout components with responsive design and form handling

---
+++ src/app/dashboard/teacher/layout.tsx
@@ -0,0 +1,50 @@
+import Menu from "@/components/Menu";
+import Navbar from "../../components/Navbar";
+import Image from "@/components/Image";
+import Link from "next/link";
+import React from "react";
+
+/**
+ * DashboardLayout component is a layout wrapper for the teacher's dashboard.
+ * It structures the page into a left sidebar and a main content area.
+ *
+ * @param {Object} props - The properties object.
+ * @param {React.ReactNode} props.children - The content to be displayed within the main content area.
+ *
+ * @returns {JSX.Element} The rendered dashboard layout component.
+ *
+ * @component
+ * @example
+ * return (
+ *   <DashboardLayout>
+ *     <YourComponent />
+ *   </DashboardLayout>
+ * )
+ */
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">SchooLama</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/dashboard/teacher/page.tsx
@@ -0,0 +1,35 @@
+import Announcements from "@/components/Announcements";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+
+/**
+ * TeacherPage component renders the dashboard for a teacher.
+ * 
+ * This component fetches the authenticated user's ID and displays a schedule
+ * and announcements section. The layout is responsive, adjusting between
+ * a single column on smaller screens and a two-column layout on larger screens.
+ * 
+ * @returns {JSX.Element} The rendered TeacherPage component.
+ */
+
+const TeacherPage = () => {
+  const { userId } = auth();
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendarContainer type="teacherId" id={userId!} />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default TeacherPage;
