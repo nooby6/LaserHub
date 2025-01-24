b260ae81cf6d643d581597ccae9819608fc5eacd
nooby6 <giftaudrine1@gmail.com>
2025-01-24 10:21:17 +0300

    Add LoginPage component with Clerk authentication; update Tailwind CSS configuration and TypeScript settings

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -1,5 +1,97 @@
-import { SignIn } from '@clerk/nextjs'
+"use client";
 
-export default function Page() {
-    return <SignIn forceRedirectUrl={} />
-}
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+/**
+ * LoginPage component renders a sign-in page for users.
+ * 
+ * This component uses the Clerk library for handling user authentication.
+ * It redirects users to a specific route based on their role after successful sign-in.
+ * 
+ * @component
+ * @returns {JSX.Element} The rendered sign-in page.
+ * 
+ * @example
+ * // Usage
+ * <LoginPage />
+ * 
+ * @remarks
+ * - The component uses `useUser` hook to get user information and authentication status.
+ * - The `useEffect` hook is used to redirect the user based on their role.
+ * - The sign-in form includes fields for username and password, with validation and error handling.
+ * - The form is styled using Tailwind CSS classes.
+ * 
+ * @dependencies
+ * - `useUser` from Clerk for user authentication.
+ * - `useRouter` from Next.js for navigation.
+ * - `SignIn` components from Clerk for rendering the sign-in form.
+ * - `Image` from Next.js for displaying the logo.
+ */
+
+
+const LoginPage = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role;
+
+    if (role) {
+      router.push(`/${role}`);
+    }
+  }, [user, router]);
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1 className="text-xl font-bold flex items-center gap-2">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            SchooLama
+          </h1>
+          <h2 className="text-gray-400">Sign in to your account</h2>
+          <Clerk.GlobalError className="text-sm text-red-400" />
+          <Clerk.Field name="identifier" className="flex flex-col gap-2">
+            <Clerk.Label className="text-xs text-gray-500">
+              Username
+            </Clerk.Label>
+            <Clerk.Input
+              type="text"
+              required
+              className="p-2 rounded-md ring-1 ring-gray-300"
+            />
+            <Clerk.FieldError className="text-xs text-red-400" />
+          </Clerk.Field>
+          <Clerk.Field name="password" className="flex flex-col gap-2">
+            <Clerk.Label className="text-xs text-gray-500">
+              Password
+            </Clerk.Label>
+            <Clerk.Input
+              type="password"
+              required
+              className="p-2 rounded-md ring-1 ring-gray-300"
+            />
+            <Clerk.FieldError className="text-xs text-red-400" />
+          </Clerk.Field>
+          <SignIn.Action
+            submit
+            className="bg-blue-500 text-white my-1 rounded-md text-sm p-[10px]"
+          >
+            Sign In
+          </SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+};
+
+export default LoginPage;
